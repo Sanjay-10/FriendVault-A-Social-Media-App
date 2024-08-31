59e6ac8ac403d199158a1293e14f1c13ed7c998e
Sanjay-10 <99119329+Sanjay-10@users.noreply.github.com>
2024-08-30 22:36:14 -0400

    day 3

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -25,4 +28,33 @@ app.use("/assets", express.static(path.join(__dirname,'public/assets')));
 
 
 // FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "public");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+});
 
+const upload = multer({storage});
+
+
+// ROUTES WITH FILES
+app.post("/auth/register" , upload.single("picture"), register);
+
+//ROUTES 
+app.use("./auth", authRoutes);
+app.use("./users", userRoutes);
+
+//MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(()=>{
+        app.listen(PORT,()=> console.log(`Server Port: ${PORT} is running perfectly` ));
+    })
+    .catch((error)=> console.log(`${PORT} did not connect`));
