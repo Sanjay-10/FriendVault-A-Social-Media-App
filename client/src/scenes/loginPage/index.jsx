bc3d1cd0c1e420da12e2c25af6be59f3a9a4af44
Sanjay-10 <99119329+Sanjay-10@users.noreply.github.com>
2024-09-03 02:09:17 -0400

    Day 6

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,11 +1,41 @@
-import { Margin } from '@mui/icons-material';
-import React from 'react'
-import Navbar from 'scenes/navbar';
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
+
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000)");
+
   return (
-    <div>
-    </div>
+    <Box>
+      <Box 
+        width="100%" 
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+      <Typography 
+        fontWeight="bold" 
+        fontSize="32px"
+        color="primary"
+        >
+          FriendVault
+        </Typography>
+        </Box>
+
+        <Box
+          width={isNonMobileScreens ? "50%" : "93%"}
+          p="2rem"
+          m="2rem auto"
+          borderRadius="1.5rem"
+          backgroundColor={theme.palette.background.alt}
+        >
+          <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
+          Welcome to Sharely! Step in to connect, share, and explore your world.
+          </Typography>
+          <Form/>
+        </Box>
+    </Box>
   )
 }
 
